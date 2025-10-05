46fafee22ddb9a194c6e7abf9ab82289a938efe7
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-10-05 07:33:34 -0400

    Inventarios

---
+++ Models/Inventarios.cs
@@ -0,0 +1,17 @@
+﻿namespace Vaperia_drink.Models
+{
+    public class Inventarios
+    {
+        public int InventarioId { get; set; }
+
+        // FK Producto
+        public int ProductoId { get; set; }
+        public Productos Producto { get; set; } = default!;
+
+        public DateTime FechaMovimiento { get; set; } = DateTime.Now;
+        public int Cantidad { get; set; }
+
+        // tipoMovimiento: "Entrada", "Salida"
+        public string TipoMovimiento { get; set; } = string.Empty;
+    }
+}
