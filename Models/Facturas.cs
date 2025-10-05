fbbae3996596843317434c12ab2e272e3be24e19
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-10-05 07:33:20 -0400

    Facturas

---
+++ Models/Facturas.cs
@@ -0,0 +1,29 @@
+﻿namespace Vaperia_drink.Models
+{
+    public class Facturas
+    {
+        public int FacturaId { get; set; }
+        public DateTime FechaEmision { get; set; } = DateTime.Now;
+
+        // FK Venta
+        public int VentaId { get; set; }
+        public Ventas Venta { get; set; } = default!;
+
+        // FK Cliente
+        public int ClienteId { get; set; }
+        public Clientes Cliente { get; set; } = default!;
+
+        // FK Usuario
+        public int UsuarioId { get; set; }
+        public Usuarios Usuario { get; set; } = default!;
+
+        public string NumeroFactura { get; set; } = string.Empty;
+        public decimal Subtotal { get; set; }
+        public decimal Impuestos { get; set; }
+        public decimal Total { get; set; }
+
+        // FK Método de Pago
+        public int MetodoPagoId { get; set; }
+        public MetodoPagos MetodoPago { get; set; } = default!;
+    }
+}
