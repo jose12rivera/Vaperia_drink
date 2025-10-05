8ad8cc608a391adaf58a849e0866b89175feeb94
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-10-05 07:34:11 -0400

    Usuarios

---
+++ Models/Usuarios.cs
@@ -0,0 +1,17 @@
+﻿namespace Vaperia_drink.Models
+{
+    public class Usuarios
+    {
+        public int UsuarioId { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Contrasena { get; set; } = string.Empty;
+        public string Rol { get; set; } = "vendedor"; // vendedor / admin
+
+        public string? FotoUrl { get; set; }
+        public byte[]? Foto { get; set; }
+
+        // Relaciones
+        public ICollection<Ventas> Ventas { get; set; } = new List<Ventas>();
+        public ICollection<Facturas> Facturas { get; set; } = new List<Facturas>();
+    }
+}
