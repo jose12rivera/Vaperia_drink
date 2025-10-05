d645cc3c7205522ee7800c8ec21791b823c34c1a
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-10-05 07:33:10 -0400

    EstadisticaVentas

---
+++ Models/EstadisticaVentas.cs
@@ -0,0 +1,15 @@
+﻿namespace Vaperia_drink.Models
+{
+    public class EstadisticaVentas
+    {
+        public int EstadisticaVentaId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int CantidadVentas { get; set; }
+        public decimal TotalVendido { get; set; }
+        public int ProductosVendidos { get; set; }
+
+        // Para reportes de inventario
+        public int Disminuye { get; set; }
+        public int Aumenta { get; set; }
+    }
+}
