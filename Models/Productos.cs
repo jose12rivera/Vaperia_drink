af8351bbb84c184b131025317fe885150543bfb2
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-10-05 07:34:02 -0400

    Productos

---
+++ Models/Productos.cs
@@ -0,0 +1,18 @@
+﻿namespace Vaperia_drink.Models
+{
+    public class Productos
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Categoria { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+        public string? FotoUrl { get; set; }
+        public byte[]? Foto { get; set; }
+
+        // Relaciones
+        public ICollection<DetalleVentas> DetallesVenta { get; set; } = new List<DetalleVentas>();
+        public ICollection<Inventarios> MovimientosInventario { get; set; } = new List<Inventarios>();
+    }
+}
