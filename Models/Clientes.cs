f3e80f179e6b9ce24884851e1faad34e387c7c84
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-10-05 07:32:33 -0400

    Clientes

---
+++ Models/Clientes.cs
@@ -0,0 +1,15 @@
+﻿namespace Vaperia_drink.Models
+{
+    public class Clientes
+    {
+        public int ClienteId { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+        public string Telefono { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        // Relaciones
+        public ICollection<Ventas> Ventas { get; set; } = new List<Ventas>();
+        public ICollection<Facturas> Facturas { get; set; } = new List<Facturas>();
+
+    }
+}
