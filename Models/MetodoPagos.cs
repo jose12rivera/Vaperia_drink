27dac6e3bf322c783b7107f4d3cc3c0db7335023
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-10-05 07:33:52 -0400

    MotedoPagos

---
+++ Models/MetodoPagos.cs
@@ -0,0 +1,11 @@
+﻿namespace Vaperia_drink.Models
+{
+    public class MetodoPagos
+    {
+        public int MetodoPagoId { get; set; }
+        public string Nombre { get; set; } = string.Empty; // Ej: Efectivo, Tarjeta, Transferencia
+
+        // Relaciones
+        public ICollection<Facturas> Facturas { get; set; } = new List<Facturas>();
+    }
+}
