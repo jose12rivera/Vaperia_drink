0f8e532cfedd3480ef439c7ec97ae0d8fc60915c
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-10-05 07:32:46 -0400

    DetalleVentas

---
+++ Models/DetalleVentas.cs
@@ -0,0 +1,23 @@
+﻿namespace Vaperia_drink.Models
+{
+    public class DetalleVentas
+    {
+        public int DetalleVentaId { get; set; }
+
+        // FK Venta
+        public int VentaId { get; set; }
+        public Ventas Venta { get; set; } = default!;
+
+        // FK Producto
+        public int ProductoId { get; set; }
+        public Productos Producto { get; set; } = default!;
+
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal SubTotal { get; set; }
+
+        // FK Usuario (quien registró el detalle)
+        public int UsuarioId { get; set; }
+        public Usuarios Usuario { get; set; } = default!;
+    }
+}
